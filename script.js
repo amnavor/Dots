1797baeddcea48ce883c5eb874f249ef9d3111f3
amnavor <amnavor@users.noreply.github.com>
2016-12-28 13:03:40 -0800

    add reset()

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
   var challengeMode = false;
-  var points = 0;
+  var round = 1;
   var playedList = [];
   var userTurn = false;
   var currentTargetIndex = 0;
   var start = false;
 
-  $("#chalButton").click(function()  {
+  $("#chalButton").click(function() {
     if (challengeMode) {
       $("#chalMode").html("off");
     } else {
@@ -15,13 +15,17 @@ $(document).ready(function() {
     }
   });
 
-  $("#reset").click(function()  {
-    points = 0;
+  $("#reset").click(reset);
+  
+  function reset() {
+    round = 1;
+    $("#screen").html("Press Start to Play");
     chal = "off";
     $("#chalMode").html("off");
     var playedList = [];
-    userTurn=false;
-  });
+    userTurn = false;
+    start = false;
+  }
 
   function add() {
     userTurn = false;
@@ -33,15 +37,20 @@ $(document).ready(function() {
     var i = 0,
       howManyTimes = playedList.length;
     var hit = playedList[i];
+
     function f() {
-      $("#"+hit).fadeOut(150).fadeIn(150);
+      if (start){
+      $("#" + hit).fadeOut(150).fadeIn(150);
       i++;
       if (i < howManyTimes) {
         hit = playedList[i];
         setTimeout(f, 1000);
-      } else if (i==howManyTimes) {
-        userTurn=true;
-        setTimeout(function() {$("td").css("background-color", "grey");}, 1000);
+      } else if (i == howManyTimes) {
+        userTurn = true;
+        setTimeout(function() {
+          $("td").css("background-color", "grey");
+        }, 1000);
+      }
       }
     }
     f();
@@ -49,44 +58,60 @@ $(document).ready(function() {
 
   $("td").click(function() {
     if (userTurn) {
+      
       var num = $(this).attr('id');
       if (playedList[currentTargetIndex] == num) {
-        $("#"+ num).css("background-color", "green");
-        setTimeout(function() { $("#"+ num).css("background-color", "grey");}, 200);
+        $("#" + num).css("background-color", "green");
+        setTimeout(function() {
+          $("#" + num).css("background-color", "grey");
+        }, 200);
         currentTargetIndex += 1;
-        points += 1;
-        if (points == 20) {
-        $("#screen").html("you win!");
-        } else if (currentTargetIndex == playedList.length) {
-        $("#screen").html("next round");
-          currentTargetIndex = 0;
-           userTurn=false;
-              setTimeout(function() {$("td").css("background-color", "grey");}, 1000);
-            setTimeout(function() {add();}, 3000);
-        }
       } else {
-         $("#"+ num).css("background-color", "red");
-       setTimeout(function() { $("#"+ num).css("background-color", "grey");}, 200);
+        $("#" + num).css("background-color", "red");
+        setTimeout(function() {
+          $("#" + num).css("background-color", "grey");
+        }, 200);
         if (!challengeMode) {
-          //replay
+          userTurn = false;
+          sequence();
         } else {
           //restart
         }
       }
-
+      
+      if (currentTargetIndex == playedList.length) {
+        round += 1;
+        if (round == 20) {
+        $("#screen").html("You Win!");
+        } else if (round == 19) {
+        $("#screen").html("Final Round!");
+        } else {
+          currentTargetIndex = 0;
+          userTurn = false;
+          setTimeout(function() {
+            $("td").css("background-color", "grey");
+          }, 1000);
+          setTimeout(function() {
+            $("#screen").html("Round " + round);
+            add();
+          }, 2000);
+        }
+      }
     }
   });
 
-$("#play").click(function()  {
-  if (!challengeMode) {
-  if (!start) {
-    add();
-  } else {
-    //repeat sequence
-  } 
-  } else {
-    //highlight challenge mode
-  }
-});
+  $("#play").click(function() {
+    if (!challengeMode) {
+      if (!start) {
+         start = true;
+           $("#screen").html("Round 1");
+        add();
+      } else {
+        //repeat sequence
+      }
+    } else {
+      //highlight challenge mode
+    }
+  });
 
 });
