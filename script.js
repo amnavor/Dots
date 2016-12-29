133794432e30bdc553f142ebbcfa7d9bc489cda8
amnavor <amnavor@users.noreply.github.com>
2016-12-29 10:07:28 -0800

    Img switch set up, sounds included

--- script.js
+++ script.js
@@ -6,25 +6,59 @@ $(document).ready(function() {
   var userTurn = false;
   var currentTargetIndex = 0;
   var start = false;
+  var imgSkull = "https://cdn3.iconfinder.com/data/icons/line/36/skull-512.png";
+  var imgNoSkull = "http://www.pd4pic.com/images/cross-black-symbol-office-sign-tick-crossed-1.png";
+  var imgPlay = "https://olin.wustl.edu/_layouts/15/OlinPublic/images/icon-play.png";
+  var imgReset= "http://iconizer.net/files/DefaultIcon_ver_0.11/orig/media-repeat-alt.png";
+
+  var audio1 = new Audio('https://upload.wikimedia.org/wikipedia/commons/a/aa/Tone_450Hz.ogg');
+  var audio2 = new Audio('https://upload.wikimedia.org/wikipedia/commons/b/bb/Tone_430-54Hz.ogg');
+  var audio3 = new Audio('https://upload.wikimedia.org/wikipedia/commons/9/91/Tone_400Hz.ogg');
+  var audio4 = new Audio('https://upload.wikimedia.org/wikipedia/commons/4/4d/Tone_380Hz.ogg');
+  var audioFail = new Audio(' https://upload.wikimedia.org/wikipedia/commons/f/f2/Fondamentale_absente.ogg');
+  var audioWin = new Audio("https://upload.wikimedia.org/wikipedia/commons/7/76/Ding_Dong_Bell.ogg");
+ 
+  function playSound(note) {
+    var audio;
+    switch (note) {
+      case 1:
+        audio = audio1;
+        break;
+      case 2:
+        audio = audio2;
+        break;
+      case 3:
+        audio = audio3;
+        break;
+      case 4:
+        audio = audio4;
+        break;
+    }
+    audio.play();
+    setTimeout(function(){
+    audio.pause();
+    }, 150);
+  }
 
   $("#chalButton").click(function() {
+    reset();
     if (challengeMode) {
-      $("#chalMode").html("off");
+      $("#chalMode").attr("src", imgNoSkull);
+      challengeMode = false;
     } else {
-      $("#chalMode").html("on");
+      $("#chalMode").attr("src", imgSkull);
+      challengeMode = true;
     }
   });
 
-  $("#reset").click(reset);
-  
   function reset() {
     round = 1;
     $("#screen").html("Press Start to Play");
-    chal = "off";
-    $("#chalMode").html("off");
-    var playedList = [];
+    playedList = [];
     userTurn = false;
     start = false;
+    currentTargetIndex = 0;
+     $("#playButton").attr("src", imgPlay);
   }
 
   function add() {
@@ -39,18 +73,19 @@ $(document).ready(function() {
     var hit = playedList[i];
 
     function f() {
-      if (start){
-      $("#" + hit).fadeOut(150).fadeIn(150);
-      i++;
-      if (i < howManyTimes) {
-        hit = playedList[i];
-        setTimeout(f, 1000);
-      } else if (i == howManyTimes) {
-        userTurn = true;
-        setTimeout(function() {
-          $("td").css("background-color", "grey");
-        }, 1000);
-      }
+      if (start) {
+        $("#" + hit).fadeOut(150).fadeIn(150);
+        playSound(hit);
+        i++;
+        if (i < howManyTimes) {
+          hit = playedList[i];
+          setTimeout(f, 1000);
+        } else if (i == howManyTimes) {
+          userTurn = true;
+          setTimeout(function() {
+            $("td").css("background-color", "grey");
+          }, 1000);
+        }
       }
     }
     f();
@@ -58,33 +93,48 @@ $(document).ready(function() {
 
   $("td").click(function() {
     if (userTurn) {
-      
+
       var num = $(this).attr('id');
       if (playedList[currentTargetIndex] == num) {
         $("#" + num).css("background-color", "green");
+        playSound(Math.round(num));
         setTimeout(function() {
           $("#" + num).css("background-color", "grey");
         }, 200);
         currentTargetIndex += 1;
       } else {
         $("#" + num).css("background-color", "red");
+        audioFail.play();
+    setTimeout(function(){
+    audioFail.pause();
+    }, 300);
         setTimeout(function() {
           $("#" + num).css("background-color", "grey");
         }, 200);
         if (!challengeMode) {
           userTurn = false;
-          sequence();
+          $("#screen").html("Try Again!");
+          setTimeout(function() {
+            $("#screen").html("Round " + round);
+            sequence();
+          }, 2000);
         } else {
-          //restart
+          $("#screen").html("Game Over");
+          setTimeout(reset, 2000);
         }
       }
-      
+
       if (currentTargetIndex == playedList.length) {
         round += 1;
-        if (round == 20) {
-        $("#screen").html("You Win!");
-        } else if (round == 19) {
-        $("#screen").html("Final Round!");
+        if (round == 21) {
+          $("#screen").html("You Win!");
+          audioWin.play();
+    setTimeout(function(){
+    audioWin.pause();
+    }, 4000);
+          setTimeout(reset, 3000);
+        } else if (round == 20) {
+          $("#screen").html("Final Round!");
         } else {
           currentTargetIndex = 0;
           userTurn = false;
@@ -101,16 +151,13 @@ $(document).ready(function() {
   });
 
   $("#play").click(function() {
-    if (!challengeMode) {
-      if (!start) {
-         start = true;
-           $("#screen").html("Round 1");
-        add();
-      } else {
-        //repeat sequence
-      }
+    if (!start) {
+      start = true;
+      $("#screen").html("Round 1");
+      $("#playButton").attr("src", imgReset);
+      setTimeout(add, 1500);
     } else {
-      //highlight challenge mode
+      reset();
     }
   });
 
